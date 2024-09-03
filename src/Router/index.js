9d8e54447ec15032d7a4a55e799812dc0348f90d
ayoolasam <obayomisamuel941@gmail.com>
2024-09-03 17:34:52 +0100

    commit

--- src/Router/index.js
+++ src/Router/index.js
@@ -6,12 +6,12 @@ const router = createRouter({
   history:createWebHistory(import.meta.env.BASE_URL),
   routes:[
     {
-      path:"/",
+      path:"/Dashboard",
       name:"DashBoard",
       component:Dashboard
     },
     {
-      path:"/Staff-Management",
+      path:"/",
       name:"StaffM",
       component:StaffManagement
     }
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -11,62 +11,62 @@
     </div>
     <ul class="list w-full">
       <li
-        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md items-ceter justify-start flex gap-2"
+        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md items-center text-[17px] justify-start flex gap-2"
       >
         <i class="fa-solid fa-house"></i>Dashboard
       </li>
       <router-link to="/Staff-Management">
         <li
-          class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md flex items-center justify-start gap-2"
+          class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md  text-[17px] flex items-center justify-start gap-2"
         >
           <i class="fa-regular fa-user"></i>Staff Management
         </li>
       </router-link>
 
       <li
-        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md items-center justify-start flex gap-2"
+        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md  text-[17px]items-center justify-start flex gap-2"
       >
         <i class="fa-solid fa-chart-simple"></i>Staff Performance
       </li>
       <li
-        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md items-center justify-start flex gap-2"
+        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md text-[17px]  items-center justify-start flex gap-2"
       >
         <i class="fa-solid fa-chart-line"></i>Reports
       </li>
       <li
-        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md items-center justify-start flex gap-2"
+        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md text-[17px] items-center justify-start flex gap-2"
       >
         <i class="fa-regular fa-clock"></i>Time Management
       </li>
       <li
-        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md items-center justify-start flex gap-2"
+        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md text-[17px] items-center justify-start flex gap-2"
       >
         <i class="fa-regular fa-images"></i>Gallery Management
       </li>
       <li
-        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md items-center justify-start flex gap-2"
+        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md text-[17px] items-center justify-start flex gap-2"
       >
         <i class="fa-solid fa-plane-departure"></i>Leave Management
       </li>
       <li
-        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md items-center justify-start flex gap-2"
+        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md text-[17px] items-center justify-start flex gap-2"
       >
         <i class="fa-regular fa-clipboard"></i> OnBoarding
       </li>
       <li
-        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md items-center justify-start flex gap-2"
+        class="py-2.5 px-2 hover:bg-tertiary hover:text-primary hover:rounded-md text-[17px] items-center justify-start flex gap-2"
       >
         <i class="fa-regular fa-door-open"></i>Exit Management
       </li>
     </ul>
     <ul class="support w-full border-tertiary border-t-2">
       <li
-        class="px-2 mt-[40px] py-2 hover:bg-tertiary hover:text-primary hover:rounded-md flex gap-2"
+        class="px-2 mt-[40px] py-2 hover:bg-tertiary hover:text-primary hover:rounded-md text-[17px] flex gap-2"
       >
         <i class="fa-solid fa-headphones"></i>Support
       </li>
       <li
-        class="px-2 py-2 hover:bg-tertiary hover:text-primary hover:rounded-md flex gap-2"
+        class="px-2 py-2 hover:bg-tertiary hover:text-primary hover:rounded-md  text-[17px] flex gap-2"
       >
         <i class="fa-solid fa-right-from-bracket"></i>Sign Out
       </li>
